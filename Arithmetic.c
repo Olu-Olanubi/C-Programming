7e818829aadf62eeae3d842a7efb019d01e89737
Olu-Olanubi <seyeolanubi@gmail.com>
2022-10-16 23:08:01 +0100

    Program to perform basic arithmetic operations on two numbers received as input

--- Arithmetic.c
+++ Arithmetic.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
+
 int main(void)
 {
 int a, b;
 
+printf("Enter two numbers: ");
+
+scanf("%d %d", &a, &b);
+
+printf("\n\nS/N\tARITHMETIC\t\tRESULT\n");
+printf("---------------------------------------\n");
+printf("1.\t(%d + %d)\t\t%d\n", a, b, (a+b));
+printf("2.\t(%d - %d)\t\t%d\n", a, b, (a-b));
+printf("3.\t(%d x %d)\t\t%d\n", a, b, (a*b));
+printf("4.\t(%d / %d)\t\t%d\n", a, b, (a/b));
+printf("5.\t(%d mod %d)\t\t%d\n", a, b, (a % b));
+printf("\n");
+
+
+return (0);
 
 }
