4ed175607c964012af149cfbde2325bae9736674
Olu-Olanubi <seyeolanubi@gmail.com>
2022-10-16 22:20:29 +0100

    Allow program to receive username from the user as input

--- 01-Hello.c
+++ 01-Hello.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+/** Program receives the name of the user as input
+*
+*/
 int main()
 {
-	printf("Hello World\n");
+	char name[99];
+	
+	//Get the name of the user
+	printf("Hello, what is your name?: ");
+	
+	//Read user name and store in the variable name
+	scanf("%s", &name); 
+	
+	//Out statement with the user name
+	printf("Hello %s, welcome to C programming", name);
 	
 	return (0);
 }
